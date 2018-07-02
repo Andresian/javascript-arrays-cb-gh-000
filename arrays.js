71cc53bfafbc188e6377f584fc60f550cda94541
Andrey Poddubny <andresiandd@gmail.com>
2018-07-02 14:09:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,24 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array, element)
+{
+  array.unshift(element)
+}
+function addElementToEndOfArray(array, element)
+{
+  array.push(element)
+} 
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  var newarray = [element]
+  array = newarray
+}
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  var newarray = array.slice(-1)
+  array = newarray
+}
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  var newarray = array.slice(1)
+  array = newarray
+}
